9589b8edb0f7bd2834591ce42565055defe9dd53
djxshin <djatlienriseup@gmail.com>
2018-12-11 15:16:18 -0500

    Created main folder

--- slavefreetoday/settings.py
+++ slavefreetoday/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_extensions'
+    'django_extensions',
+    'main'
 ]
 
 MIDDLEWARE = [
