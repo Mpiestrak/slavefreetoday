38450aff0a699295719b7eea6d142f15a98ca619
djxshin <djatlienriseup@gmail.com>
2018-12-15 16:05:31 -0500

    half way thru homepage

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -21,7 +21,50 @@ class Homepage extends Component {
                             <div className='title2'>A multidisciplinary exploration of human trafficking solutions</div>
                             <button className='hpbutton'>Create a FREE account to access the Journal</button>
                         </div>
-                        <div className='two'>two </div>
+                        <div className='two'>
+                           <div className='hptitle3'>RESOURCES</div>
+
+                            <div className='hptitle4'>International collection of resources for researchers, practitioners, students, policy makers and more</div>
+
+                          <div className='hpIconContain'>
+
+                          <a className='hpIconOne'>
+                              <div className='iconPic'>
+
+                              </div>
+                              <div className='iconLabel'>
+                                  NGOS
+                              </div>
+
+                          </a>
+                           <a className='hpIconTwo'>
+                                 <div className='iconPic2'>
+
+                              </div>
+                                <div className='iconLabel'>
+                                    Reports
+                              </div>
+                           </a>
+                            <a className='hpIconThree'>
+                                  <div className='iconPic3'>
+
+                                </div>
+                                   <div className='iconLabel'>
+                                    Training
+                                </div>
+                            </a>
+                             <a className='hpIconFour'>
+                                   <div className='iconPic4'>
+
+                              </div>
+                                    <div className='iconLabel4'>
+                                    Emergency Contacts
+                                </div>
+                             </a>
+
+
+                          </div>
+                        </div>
                         <div className='three'> three</div>
                         <div className='four'> four</div>
                         <div className='five'> five</div>
--- client/src/components/MemberList.js
+++ client/src/components/MemberList.js
@@ -14,3 +14,4 @@ class MemberList extends Component {
 }
 
 export default MemberList;
+
