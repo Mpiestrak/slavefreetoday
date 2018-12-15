3abc1afff4c60228316f261679804eb0e5fa1a79
djxshin <djatlienriseup@gmail.com>
2018-12-15 17:35:45 -0500

    homepage component styling complete

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -65,9 +65,119 @@ class Homepage extends Component {
 
                           </div>
                         </div>
-                        <div className='three'> three</div>
-                        <div className='four'> four</div>
-                        <div className='five'> five</div>
+
+                        <div className='three'>
+                              <div className='hptitle5'>MULTIMEDIA</div>
+
+                            <div className='hptitle6'>Our very own podcast and blog, and a collection of relevant books and videos.</div>
+
+                          <div className='hpIconContain'>
+
+                          <a className='hpIconOne'>
+                              <div className='iconPic5'>
+
+                              </div>
+                              <div className='iconLabel2'>
+                                  Podcast
+                              </div>
+
+                          </a>
+                           <a className='hpIconTwo'>
+                                 <div className='iconPic6'>
+
+                              </div>
+                                <div className='iconLabel2'>
+                                    Blog
+                              </div>
+                           </a>
+                            <a className='hpIconThree'>
+                                  <div className='iconPic7'>
+
+                                </div>
+                                   <div className='iconLabel2'>
+                                    Books
+                                </div>
+                            </a>
+                             <a className='hpIconFour'>
+                                   <div className='iconPic8'>
+
+                              </div>
+                                    <div className='iconLabel2'>
+                                    Videos
+                                </div>
+                             </a>
+
+
+                          </div>
+
+
+                        </div>
+                        <div className='four'>
+                                  <div className='hptitle3'>CALENDER</div>
+
+                            <div className='hptitle4'>A list of global events and conferences related to issues around slavery. Add events that you know of to the calendar.</div>
+
+                          <div className='hpIconContain'>
+
+                          <a className='hpIconOne'>
+                              <div className='iconPic9'>
+
+                              </div>
+                              <div className='iconLabel5'>
+                                  View Events
+                              </div>
+
+                          </a>
+                           <a className='hpIconTwo'>
+                                 <div className='iconPic10'>
+
+                              </div>
+                                <div className='iconLabel5'>
+                                    Add Events
+                              </div>
+                           </a>
+
+
+
+                          </div>
+
+
+
+                        </div>
+                        <div className='five'>
+
+                         <div className='hptitle5'>ABOUT US</div>
+
+                            <div className='hptitle6'>Learn more about our mission and who we are.</div>
+
+                          <div className='hpIconContain'>
+
+                          <a className='hpIconOne'>
+                              <div className='iconPic11'>
+
+                              </div>
+                              <div className='iconLabel2'>
+                                  Mission
+                              </div>
+
+                          </a>
+                           <a className='hpIconTwo'>
+                                 <div className='iconPic12'>
+
+                              </div>
+                                <div className='iconLabel2'>
+                                   Leadership
+                              </div>
+                           </a>
+
+
+
+                          </div>
+
+
+
+
+                        </div>
 
 
                     </div>
