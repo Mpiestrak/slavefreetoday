4c0f8b3fcaf27fd6860624c3d8613cafa08020a4
djxshin <djatlienriseup@gmail.com>
2018-12-17 18:46:27 -0500

    donation paypal linked and looks pretty

--- client/src/components/Donations.js
+++ client/src/components/Donations.js
@@ -28,15 +28,12 @@ class Donations extends Component {
                 <div className='title'>DONATE TODAY</div>
 
                 <div className='body'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.
+                        Please click on the button below (PAYPAL) to be directed to slavefreetoday.org Payal Account.
                 </div>
                 <Button type='button' className='button'  onClick={this.toggle}>
-                <div className='buttonbody'>
-                            GIVE ONLINE NOW VIA PAYP
-                </div>
                 
                 </Button>
-               
+                <div className="clickhere">CLICK ABOVE</div>
                     
                 <div className='dollarcontainer'>
 
@@ -172,13 +169,15 @@ class Donations extends Component {
         <div>
         
         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-          <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+          <ModalHeader toggle={this.toggle}>Donate to SlaveFreeToday.org</ModalHeader>
           <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+           We appreciate your interest in our non-profit organization.<br/>
+           We are currently running our charitable donation through PAYPAL.COM <br/>
+           Please click on the button below to be directed to our company through PAYPAL.
           </ModalBody>
           <ModalFooter>
-            <Button color="primary" href="https://www.paypal.me/DJshinATLANTA" target="_blank">Do Something</Button>{' '}
-            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+            <Button color="primary" href="https://www.paypal.me/DJshinATLANTA" target="_blank">DIRECT LINK TO SlaveFreeToday's PAYPAL Account</Button>{' '}
+  
           </ModalFooter>
         </Modal>
       </div>
